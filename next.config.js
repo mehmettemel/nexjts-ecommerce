3e0434aceab78fee597d8db3e1628ddee95688df
mehmet temel <temel1072@hotmail.com>
2021-09-06 11:18:04 +0300

    come onn

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    BASE_URL: 'http://localhost:3000',
+    BASE_URL: 'https://temel-ecommerce.vercel.app',
     MONGODB_URL:
       'mongodb+srv://temel:temel123@cluster0.jc4ws.mongodb.net/temelEcommerce?retryWrites=true&w=majority',
     ACCESS_TOKEN_SECRET: 'vC7g/>#Vct/]Y6',
